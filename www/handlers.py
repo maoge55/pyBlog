536d09b4a83a3e4f9aad11a6e6872da6e3753299
maoge55 <1126642524@qq.com>
2021-04-10 15:55:05 +0800

    day5

---
+++ www/handlers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Mao ZhongJie'
+
+' url handlers '
+
+import re, time, json, logging, hashlib, base64, asyncio
+
+from aiohttp import web
+
+from coroweb import get, post
+from apis import APIValueError, APIResourceNotFoundError
+
+from model import User
+
+COOKIE_NAME = 'awesession'
+
+@get('/')
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    return {
+        '__template__': 'index.html',
+        'pid':'123',
+        'cname':456
+    }
--- www/test.py
+++ www/test.py
@@ -1,10 +1,15 @@
 import asyncio, logging; logging.basicConfig(level=logging.INFO)
-import aiomysql,time
+import aiomysql,time,inspect
 from orm import __pool,select,execute,create_pool,Model,StringField,IntegerField,BooleanField
-from model import User,Blog,Comment
-
-
 loop= asyncio.get_event_loop()
+
+import requests,json
+#r = requests.get('http://localhost:9000', params={'q': 'python', 'cat': '1001'})
+data={'form_email': 'abc@example.com', 'form_password': '123456'}
+data_json = json.dumps(data)
+r = requests.post('http://localhost:9000/maoge/abc',json=data)
+print(r.content)
+
 
 async def test1():
     await create_pool(loop,user='www-data',password='www-data',db='awesome')
@@ -21,4 +26,29 @@ async def test2():
     rs=await select(sql,(False,))
     print(rs)
 
-loop.run_until_complete(test1())
+#loop.run_until_complete(test1())
+
+
+
+def get_required_kw_args(fn):
+    args=[]
+    params=inspect.signature(fn).parameters
+    for name,param in params.items():
+        if param.kind==inspect.Parameter.KEYWORD_ONLY and param.default==inspect.Parameter.empty:
+            args.append(name)
+
+    return tuple(args)
+
+def get_named_kw_args(fn):
+    args=[]
+    params=inspect.signature(fn).parameters
+    for name,param in params.items():
+        if param.kind==inspect.Parameter.KEYWORD_ONLY:
+            args.append(name)
+    
+    return tuple(args)
+
+def maoge(a,b,*c,ol,jk,pp,pid=123,name='maoge',**kw):
+    pass
+
+
