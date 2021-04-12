664403c1dadd83ae9f3fb6c6a55f3ca3122c3b61
maoge55 <1126642524@qq.com>
2021-04-12 09:37:26 +0800

    day8

--- www/handlers.py
+++ www/handlers.py
@@ -12,20 +12,24 @@ from aiohttp import web
 from coroweb import get, post
 from apis import APIValueError, APIResourceNotFoundError
 
-from model import User
+from model import User,Blog
 
 COOKIE_NAME = 'awesession'
 
-@get('/{sss}')
-def index(request):
+@get('/')
+async def index(request):
     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
     return {
-        '__template__': 'index.html',
-        'pid':'123',
-        'cname':456
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
 
-@get('/')
+@get('/maoge')
 async def test(request):
     users=await User.findAll()
 
