e20f7466097aeee3127bad656738f22f56c888d9
maoge55 <1126642524@qq.com>
2021-04-12 20:55:47 +0800

    day10.2
    Signed-off-by: maoge55 <1126642524@qq.com>

--- www/handlers.py
+++ www/handlers.py
@@ -10,11 +10,12 @@ import re, time, json, logging, hashlib, base64, asyncio
 from aiohttp import web
 
 from coroweb import get, post
-from apis import APIValueError, APIResourceNotFoundError
+from apis import APIValueError, APIResourceNotFoundError,APIError
 
-from model import User,Blog
-
+from model import User, Comment, Blog, next_id
+from config import configs
 COOKIE_NAME = 'awesession'
+_COOKIE_KEY = configs.session.secret
 
 @get('/')
 async def index(request):
@@ -29,11 +30,115 @@ async def index(request):
         'blogs': blogs
     }
 
-@get('/maoge')
-async def test(request):
-    users=await User.findAll()
+@get('/api/users')
+async def api_get_users():
+    users = await User.findAll(orderBy='created_at desc')
+    return dict(users=users)
 
-    return{
-        '__template__':'test.html',
-        'users':users
+def user2cookie(user,max_age):
+    '''
+    Generate cookie str by user.
+    '''
+    # build cookie string by: id-expires-sha1
+    expires=str(int(time.time()+max_age))
+    s=f'{user.id}-{user.passwd}-{expires}-{_COOKIE_KEY}'
+    L=[user.id,expires,hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    return '-'.join(L)
+
+async def cookie2user(cookie_str):
+    '''
+    Parse cookie and load user if cookie is valid.
+    '''
+    if not cookie_str:
+        return None
+    try:
+        L=cookie_str.split('-')
+        if len(L)!=3:
+            return None
+        uid,expires,sha1=L
+        if(int(expires)<time.time()):
+            return None
+        user=await User.find(uid)
+        if user is None:
+            return None
+        s=f'{uid}-{user.passwd}-{expires}-{_COOKIE_KEY}'
+        if sha1!=hashlib.sha1(s.encode('utf-8')).hexdigest():
+            logging.info('invalid sha1')
+            return None
+        user.passwd='******'
+        return user
+    except Exception as e:
+        logging.exception(e)
+        return None
+
+@get('/register')
+def register():
+    return {
+        '__template__': 'register.html'
     }
+
+@get('/signin')
+def signin():
+    return {
+        '__template__': 'signin.html'
+    }
+
+@post('/api/authenticate')
+async def authenticate(*, email, passwd):
+    if not email:
+        raise APIValueError('email','Invalid email')
+    if not passwd:
+        raise APIValueError('passwd','Invalid passwd')
+    users=await User.findAll('email=?',[email])
+    if len(users)==0:
+        raise APIValueError('email','Email not exist.')
+    user=users[0]
+    #check passwd:
+    sha1=hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(passwd.encode('utf-8'))
+    if user.passwd!=sha1.hexdigest():
+        raise APIValueError('passwd','Invalid password.')
+    # authenticate ok, set cookie:
+    r=web.Response()
+    r.set_cookie(COOKIE_NAME,user2cookie(user,86400),max_age=86400,httponly=True)
+    user.passwd='******'
+    r.content_type='application/json'
+    r.body=json.dumps(user,ensure_ascii=False).encode('utf-8')
+    return r
+
+@get('/signout')
+def signout(request):
+    referer = request.headers.get('Referer')
+    r = web.HTTPFound(referer or '/')
+    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
+    logging.info('user signed out.')
+    return r
+
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+@post('/api/users')
+async def api_register_user(*, email, name, passwd):
+    if not name and not name.strip():
+        raise APIValueError('name')
+    if not email and not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd and not _RE_SHA1(passwd):
+        raise APIValueError('passwd')
+    users=await User.findAll('email=?',[email])
+    if len(users)>0:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+    uid=next_id()
+    sha1_passwd=f'{uid}:{passwd}'
+    user=User(id=uid,name=name.strip(),email=email,passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),image=f'http://www.gravatar.com/avatar/{hashlib.md5(email.encode("utf-8")).hexdigest()}?d=mm&s=120')
+    await user.save()
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd='******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+
--- www/test.py
+++ www/test.py
@@ -1,22 +1,22 @@
 import asyncio, logging; logging.basicConfig(level=logging.INFO)
 import aiomysql,time,inspect
-from orm import __pool,select,execute,create_pool,Model,StringField,IntegerField,BooleanField,User
+from orm import __pool,select,execute,create_pool,Model,StringField,IntegerField,BooleanField
+from model import User
 loop= asyncio.get_event_loop()
 
 import requests,json
-#r = requests.get('http://localhost:9000', params={'q': 'python', 'cat': '1001'})
-data={'form_email': 'abc@example.com', 'form_password': '123456'}
-data_json = json.dumps(data)
-r = requests.post('http://localhost:9000/maoge/abc',json=data)
-print(r.content)
+#r = requests.get('http://localhost:9000/api/users', params={'q': 'python', 'cat': '1001'})
+# data={'form_email': 'abc@example.com', 'form_password': '123456'}
+# data_json = json.dumps(data)
+# r = requests.post('http://localhost:9000/maoge/abc',json=data)
+# ooo=r.json()
+# print(ooo)
+# print(type(ooo))
 
 
 async def test1():
     await create_pool(loop,user='www-data',password='www-data',db='awesome')
     #rss=await User.find(4)
-    maoge=User(email='990835192@qq.com',passwd='123456',image='about:blank',name='junzi')
-    print(time.time())
-    await maoge.save()
     rss=await User.findAll()
     print(rss)
 
@@ -26,7 +26,7 @@ async def test2():
     rs=await select(sql,(False,))
     print(rs)
 
-#loop.run_until_complete(test1())
+loop.run_until_complete(test1())
 
 
 
